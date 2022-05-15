3618005a8e26d7939e5c6ac1e81c32b5fe9915ec
hannal <kay@hannal.net>
2022-05-15 18:16:55 +0900

    ADD ID 중복에 따른 회원가입 서비스 실패 시나리오.

--- bereal/apiserver/accounts/tests/test_signup_service.py
+++ bereal/apiserver/accounts/tests/test_signup_service.py
@@ -23,3 +23,17 @@ def test_유효한_ID와_PW를_제출하면_ID_PW를_저장하고_회원_가입_
 
     result = signup(payload, repository)
     assert result['code'] == 201
+
+
+def test_중복된_ID를_제출하면_중복_ID_오류_응답을_한다(fixtures):
+    repository = fixtures['repository']
+    payload = {
+        'username': 'duplicated',
+        'password': 'test',
+    }
+    result = signup(payload, repository)
+    assert result['code'] == 201
+
+    result = signup(payload, repository)
+    assert result['code'] == 400
+    assert result['reason'] == 'duplicated-username'
