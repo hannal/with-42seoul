49dcedfd571fced01eec81ef4ca5d8898f23f82d
hannal <kay@hannal.net>
2022-05-15 19:19:17 +0900

    REFACTOR 회원가입 service 에 repository 별 테스트 반영.

--- bereal/apiserver/accounts/tests/test_signup_service.py
+++ bereal/apiserver/accounts/tests/test_signup_service.py
@@ -1,21 +1,32 @@
 import pytest
 
 from accounts.services import signup
-from accounts.repositories import Repository
+from accounts.repositories import FileRepository, DBRepository
 
 
 @pytest.fixture
 def fixtures():
-    storage_name = 'accounts.json'
-    repository = Repository(storage_name)
-    yield {
-        'repository': repository,
+    result = {
+        'file_repository': FileRepository('accounts.json'),
+        'db_repository': DBRepository(),
     }
-    repository.unlink_storage()
+    yield result
+    result['file_repository'].unlink_storage()
 
 
-def test_유효한_ID와_PW를_제출하면_ID_PW를_저장하고_회원_가입_완료_응답을_한다(fixtures):
-    repository = fixtures['repository']
+repository_parametrize = pytest.mark.parametrize(
+    'repository_key',
+    [
+        'file_repository',
+        'db_repository',
+    ]
+)
+
+
+@repository_parametrize
+@pytest.mark.django_db
+def test_유효한_ID와_PW를_제출하면_ID_PW를_저장하고_회원_가입_완료_응답을_한다(fixtures, repository_key):
+    repository = fixtures[repository_key]
     payload = {
         'username': 'test',
         'password': 'test',
@@ -25,8 +36,10 @@ def test_유효한_ID와_PW를_제출하면_ID_PW를_저장하고_회원_가입_
     assert result['code'] == 201
 
 
-def test_중복된_ID를_제출하면_중복_ID_오류_응답을_한다(fixtures):
-    repository = fixtures['repository']
+@repository_parametrize
+@pytest.mark.django_db
+def test_중복된_ID를_제출하면_중복_ID_오류_응답을_한다(fixtures, repository_key):
+    repository = fixtures[repository_key]
     payload = {
         'username': 'duplicated',
         'password': 'test',
